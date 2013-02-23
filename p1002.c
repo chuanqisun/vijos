9e32fe8f46934747fb5db0c8728fc7a9aa3c9d30
Chuanqi Sun <henrysun918@gmail.com>
2013-02-23 15:59:03 -0500

    x

--- p1002.c
+++ p1002.c
@@ -1,92 +1,97 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <string.h>
 
 
 int main(int argc, char *argv[]){
 
 	int L, S, T, M;
-	int stone[101];
-	int m,i,j, step,t;
+    int stone_loc[101];
+	int stone_mark[9000];
+    int stone_count[9000];
+	int m,i,j,step,t,shift,max_distance;
 	int min_stone;
 	
 	//input
 	scanf("%d", &L);
 	scanf("%d %d %d", &S,&T,&M);
-	for (m=0; m<M; m++){
-		scanf("%d",&i);
-		stone[m]=i;
-	}
 
+
+    //init 
+    memset(stone_mark, 0, 9000);
+    memset(stone_loc, 0, 101);
+    memset(stone_count, 101, 9000);
+    stone_count[0]=0;
+    shift=0;
+
+
+
+    //compression length
+    max_distance=S * (S-1) + T;
+
+
+    //read in stone location
+	for (m=1; m<=M; m++){
+		scanf("%d",&i);
+        stone_loc[m]=i;
+	}
 	
-	//sort stones for better performance
-	for (i=0; i<M-1; i++){
-		for (j=0; j<M-1-i; j++){
-			if (stone[j]>stone[j+1]){
-				t=stone[j];
-				stone[j]=stone[j+1];
-				stone[j+1]=t;
+	//sort stones
+	for (i=0; i<M; i++){
+		for (j=0; j<M-i; j++){
+			if (stone_loc[j]>stone_loc[j+1]){
+				t=stone_loc[j];
+				stone_loc[j]=stone_loc[j+1];
+				stone_loc[j+1]=t;
 			}
 		}
 	}	
+    
 
+    min_stone=0;
+    //directly compute min stones
+    if (S==T) {
+        for (i=1; i<=M; i++) {
+           if (stone_loc[i] % S == 0) {
+               min_stone++;
+           }
+        }
+        printf("%d", min_stone);
+        return 0;
+    }
+    
+    //compress distance to a maximum of max_distance
+    for (i=1; i<=M; i++){
+        shift=stone_loc[i]-stone_loc[i-1]-max_distance;
+        if (shift > 0) {
+            for (j=i; j<=M; j++){
+                stone_loc[j]-=shift;
+            }
+        }
+        stone_mark[stone_loc[i]]=1;
+    }
+    
+    //compress L according to last stone
+    L=stone_loc[M]+max_distance;
 
 	int cur;
-
-	int window[11];
-	int window_stone[11];
-
-	min_stone=0;
 	
-	for (i=0; i<=T; i++){
-		window_stone[i]=0;
-		for (j=0; j<M; j++){
-			if (stone[j]==i){
-				window_stone[i]=1;
-				min_stone=j;
-			}
-		}
-		window[i]=101;
-	}
-	
-	window[0]=0;	
-
+    //DP one time iteration through the array
 
 	for (cur=0; cur<L; cur++){
-		if (window[0]<=100){
-			for (step=S;step<=T;step++){
-				if (window[0]+ (window_stone[step]) < window[step]) {
-					window[step]=window[0]+window_stone[step];
-				}
-			}
-		}
-		for (i=0; i<T; i++){
-			window[i]=window[i+1];
-			window_stone[i]=window_stone[i+1];
-		}
+	    for (step=S;step<=T;step++){
+            if (stone_count[cur] + stone_mark[cur + step] < stone_count[cur + step]) {
+                stone_count[cur + step] = stone_count[cur] + stone_mark[cur + step];
+            }
+        }
+    }
+    
+    min_stone=101;
+    for (i=L; i<L+T; i++) {
+		if (stone_count[i] < min_stone){
+            min_stone = stone_count[i];
+        }
+    }
 
-
-		window_stone[T]=0;
-		for (j=min_stone; j<M; j++){
-			if(stone[j]>cur+T+1){
-				min_stone=j;
-				break;
-			}else if (stone[j]==cur+T+1){
-				window_stone[T]=1;
-				min_stone=j+1;
-				break;
-			}
-		}
-		window[T]=101;
-	}
-		
-	int min_step=101;
-	for (i=0; i<T; i++){
-		if (window[i]<min_step) {
-			min_step=window[i];
-		}
-	}
-	printf("%d", min_step);
-	return 0;
+    printf("%d", min_stone);
 }
 
